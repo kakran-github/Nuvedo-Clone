4a1947dcbe3b167da00db1730fe329e18f02fc18
Yuvraj Chaudhary <yuvr4aj@gmail.com>
2024-03-29 14:16:51 +0530

    animations

--- script.js
+++ script.js
@@ -25,3 +25,38 @@ function slideImage(){
         slide.style.transform=`translateX(-${counter*100}%)`
     })
 }
+
+const scroller = document.querySelector('.scroller-page1');
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0) {
+    scroller.classList.add('scrolled');
+  } else {
+    scroller.classList.remove('scrolled');
+  }
+});
+
+window.addEventListener('scroll', function() {
+    var nav = document.querySelector('.nav');
+    
+    if (window.scrollY > 0) {
+      nav.classList.add('scrollednav');
+    } else {
+      nav.classList.remove('scrollednav');
+    }
+  });
+
+  window.addEventListener('scroll', function() {
+    var nav = document.querySelector('.nav');
+    var navImg = document.querySelector('.nav img'); // Select the image within the navigation
+
+    if (window.scrollY > 60) {
+      nav.classList.add('scrollednav1'); // Add the 'fixed' class when scrolling begins
+      navImg.classList.add('invert'); // Add the 'invert' class to the image when scrolling begins
+    } else {
+      nav.classList.remove('scrollednav1'); // Remove the 'fixed' class when scrolling back to the top
+      navImg.classList.remove('invert'); // Remove the 'invert' class from the image when scrolling back to the top
+    }
+  });
+
+ 
